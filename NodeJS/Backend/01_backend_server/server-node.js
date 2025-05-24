6d164b0e5f9794692de5a5df63bc22ff6d2e6a8a
Akhil Chandewar <akhil.chandewar00@gmail.com>
2025-05-25 00:23:55 +0530

    backend launchpad

---
+++ NodeJS/Backend/01_backend_server/server-node.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!\n');
+  }
+  else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
