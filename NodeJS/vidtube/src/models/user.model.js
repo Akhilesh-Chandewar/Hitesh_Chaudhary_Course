c843db6dac6690c9106a11aa7aaf0da3abc3b04d
Akhil Chandewar <akhil.chandewar00@gmail.com>
2025-05-26 16:03:13 +0530

    added mongoose hooks and methods

--- NodeJS/vidtube/src/models/user.model.js
+++ NodeJS/vidtube/src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { use } from "react";
 
 const userSchema = new Schema(
     {
@@ -53,5 +56,30 @@ const userSchema = new Schema(
     }
 );
 
+userSchema.pre('save' , async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+}
+
+userSchema.methods.generateAccessToken = function() {
+   returnjwt.sign(
+        { userId: this._id, username: this.username },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRATION }
+    );
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        { userId: this._id, username: this.username },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRATION }
+    );
+}
 const User = mongoose.model('User', userSchema);
 export default User;
